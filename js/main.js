04f2c84bca745b9966e3e713ec0726b72455d233
Imal hasaranga <imaa95@gmail.com>
2016-07-02 20:50:12 +0530

    no message

--- js/main.js
+++ js/main.js
@@ -1,66 +1,114 @@
-function createclasses(){
+function createclasses() {
 
-	var json = document.getElementById("sourcedata").value;
-	if(json && json.trim() != ""){
-		try{
-			var allclassses = [];
+    var json = document.getElementById("sourcedata").value;
+    var baseclass = document.getElementById("inputclassname");
+    var modname = document.getElementById("inputmodname");
+    var baseClassName = baseclass.value.trim() == "" ? "Base" : baseclass.value.trim();
+    var modulename = modname.value.trim() == "" ? "JsClassTest" : modname.value.trim();
 
-
-			json = JSON.parse(json);
-			breakDown(json,"Base");
-
-			for(var i in allclassses){
-				console.log(JSON.stringify(allclassses[i])+" "+i );
-			}
-
-			function breakDown(object,name){
-				for(var attr in object){
-					if(typeof object[attr] == "object"){
-						var obj = object[attr];
-						if(Object.prototype.toString.call(obj) === '[object Array]' && obj.length > 0){
-							obj = obj[0];
-						}
-						object[attr] = "";
-						breakDown(obj,attr);
-					}
-				}
-				allclassses.push({name : name, obj : object });
-			}
-
-			var totalstring = "";
-			for(var y in allclassses){
-				var classob = createClassSignature(allclassses[y]);				
-				totalstring = "/*-------------------------------------------------------*/\n"+classob + totalstring;
-			}
-			document.getElementById("resultdata").value = totalstring;
+    if (json && json.trim() != "") {
+        try {
+            var allclassses = [];
+            var usedClasses = [];
 
 
-			function createClassSignature(classob){
-				var maincode = "var "+(classob.name).toLowerCase()+" =  function(){\n" ;
-				var methods = "";
-				for(var attr in classob.obj){
-					maincode += "\tthis."+attr+";\n";
-					methods  += (classob.name).toLowerCase()+".prototype.get"+capitalizeFirstLetter(attr)+"(){\n";
-					methods  +=  "\treturn this."+attr+"; \n";
-					methods  +=  "}\n";
-					methods  += (classob.name).toLowerCase()+".prototype.set"+capitalizeFirstLetter(attr)+"("+attr+"){\n";
-					methods  +=  "\tthis."+attr+"="+attr+"; \n";
-					methods  +=  "}\n";
-				}
-				return (maincode+"}\n"+methods);
-			}
-			
-			function capitalizeFirstLetter(string) {
-			    return string.charAt(0).toUpperCase() + string.slice(1);
-			}
+            json = JSON.parse(json);
+            breakDown(json, baseClassName);
+            for (var y in allclassses) {
+                var classob = createClassSignature(allclassses[y]);
+                cleanUp(classob);
+            }
+            var totalstring = "";
+            for (var y in usedClasses) {
+                totalstring =  (y == usedClasses.length-1 ? "" : "\n\n") +"/*************"+usedClasses[y].class_name+"**************/\n" + usedClasses[y].signature + totalstring;
+            }
+            document.getElementById("resultdata").innerHTML  = totalstring;
+            removeClass(document.getElementById("resultdata"),"prettyprinted");
+            prettyPrint();
+
+            function cleanUp(classobx) {
+                var found = false;
+                for (var i in usedClasses) {
+                    var usedclass = usedClasses[i];
+                    if (usedclass.class_name == classobx.class_name) {
+                        found = true;
+                        if (classobx.attrcounter >= usedclass.attrcounter) {
+                            usedClasses[i] = classobx;
+                        }
+                    }
+                }
+                if (!found) {
+                    usedClasses.push(classobx);
+                }
+            }
+
+            function breakDown(object, name) {
+                for (var attr in object) {
+                    if (typeof object[attr] == "object") {
+                        var obj = object[attr];
+                        if (Object.prototype.toString.call(obj) === '[object Array]' && obj.length > 0) {
+                            obj = obj[0];
+                        }
+                        object[attr] = "";
+                        if(typeof obj == "object"){
+                            breakDown(obj, attr);
+                        }
+                    }
+                }
+                allclassses.push({ name: name, obj: object });
+            }
 
 
-			//console.log(json);
-		}catch(err) {
-			console.log(err.message);
-		}
+            function createClassSignature(classob) {
+                var classname = capitalizeFirstLetter((classob.name).toLowerCase());
+                var maincode = "angular.module(\""+modulename+"\").factory(\""+classname+"\", [\"BaseObject\", function(BaseObject) {\n";
+                    maincode  += "\tvar " + classname + " =  function(){\n";
+                var methods = "";
+                var counter = 0;
+                for (var attr in classob.obj) {
+                    maincode += "\t\tthis." + attr + ";\n";
+                    methods += "\t"+(classname) + ".prototype.get" + capitalizeFirstLetter(attr) + "= function(){\n";
+                    methods += "\t\treturn this." + attr + "; \n";
+                    methods += "\t};\n";
+                    methods += "\t"+(classname) + ".prototype.set" + capitalizeFirstLetter(attr) + "= function(" + attr + "){\n";
+                    methods += "\t\tthis." + attr + "=" + attr + "; \n";
+                    methods += "\t};\n";
+                    ++counter;
+                }
+                var inheritance = "\n\tBaseObject.inherit("+classname+", function(mapper) {\n"+
+                				  "\t\t/* Use mapper object to register your mappings */\n"+
+							      "\t\t//mapper.addClassMapping(\"friend\", \"Friend\");\n"+
+							      "\t\t//mapper.addAttrMapping(\"name_one\", \"name\");\n"+
+							      "\t});\n";
+				var clsingtag = "\treturn "+classname+";\n"+
+								"}]);";
+                var finalstr  = maincode + "\t}"+inheritance+ methods + clsingtag;
+                	
 
-	}
+                return { class_name: classname, signature: finalstr, attrcounter: counter };
+            }
+
+            function capitalizeFirstLetter(string) {
+                return string.charAt(0).toUpperCase() + string.slice(1);
+            }
 
 
+            //console.log(json);
+        } catch (err) {
+            console.log(err.message);
+        }
+
+    }
+
+
+}
+
+
+function removeClass(el, className) {
+  if (el.classList)
+    el.classList.remove(className)
+  else if (hasClass(el, className)) {
+    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
+    el.className=el.className.replace(reg, ' ')
+  }
 }
